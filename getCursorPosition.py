8f7620726fdf52c1db8ab3499468059b777e504e
thiagocastro1721 <58869140+thiagocastro1721@users.noreply.github.com>
2025-10-19 00:22:39 -0300

    Create getCursorPosition.py

---
+++ getCursorPosition.py
@@ -0,0 +1,7 @@
+import pyautogui
+import time
+screenWidth, screenHeight = pyautogui.size()
+print(pyautogui.size())
+time.sleep(5)
+currentMouseX, curentMouseY = pyautogui.position()
+print(pyautogui.position())
